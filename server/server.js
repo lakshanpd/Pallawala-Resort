d69883cd07fcccdd69dbcac84c8e788c43585c65
danuka <lakshanpd.cse21@gmail.com>
2024-02-08 23:20:35 +0530

    routes and db connection separated from server.js

--- server/server.js
+++ server/server.js
@@ -1,80 +1,19 @@
-const express = require('express')
-const mysql = require('mysql2')
-const cors = require('cors');
-require('dotenv').config();
-const app = express()
-const PORT = 3001;
+const express = require('express');
+const app = express();
+const db = require('./db'); // Import the database connection
+const routes = require('./routes'); // Import the route definitions
 
-app.use(cors());
+// Middleware for parsing JSON bodies
+app.use(express.json());
 
-const db = mysql.createConnection({
-    host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE
-})
+// Use the routes defined in routes.js
+app.use('/', routes);
 
-// app.get('/api/description', (req, res) => {
-//     db.query('SELECT Description FROM Room WHERE ?', [cottageNumber], (err, data) => {
-//         if (err) return res.json(err);
-//         console.log(typeof data)
-//         return res.json(data);
-//     })
-// })
-
-// Define a route that accepts a parameter
-app.get('/api/:cottageNumber', (req, res) => {
-    // Retrieve the parameter from the URL
-    const cottageNumber = req.params.cottageNumber;
-  
-    db.query('SELECT Description FROM Room WHERE RoomID = ?', [cottageNumber], (err, data) => {
-        if (err) return res.json(err);
-        return res.json(data);
-    })
-  });
-
-app.get('/api/review/contents', (req, res) => {
-    db.query('SELECT Content FROM Review', (err, data) => {
-        if (err) return res.json(err);
-        return res.json(data);
-    })
-})
-
-app.get('/api/review/rating', (req, res) => {
-    db.query('SELECT Rating FROM Review', (err, data) => {
-        if (err) return res.json(err);
-        return res.json(data);
-    })
-})
-
-app.get('/api/review/customer_names', (req, res) => {
-
-    const sql_query = `SELECT FirstName, LastName FROM Customer
-    RIGHT JOIN 
-    (SELECT Booking.BookingRef, Booking.CustomerID FROM Booking
-    RIGHT JOIN Review 
-    ON Review.BookingRef = Booking.BookingRef) AS intermediate
-    ON Customer.CustomerID = intermediate.CustomerID;`
-
-    db.query(sql_query, (err, data) => {
-        if (err) return res.json(err);
-        console.log(data)
-        return res.json(data);
-    })
-})
-
-app.get('/api/cottage/cottage_prices', (req, res) => {
-
-    const sql_query = `SELECT Price FROM Room WHERE IsCottage=True;`;
-
-    db.query(sql_query, (err, data) => {
-        if (err) return res.json(err);
-        console.log(data)
-        return res.json(data);
-    })
-})
+// Start the server
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
-    console.log('successfull')
-})
+  console.log(`Server is listening on port ${PORT}`);
+});
+
 
